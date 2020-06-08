6d70b4af5244241e4aaba78452353e0dab9023be
dolev1894 <dolevda@ac.sce.ac.il>
2020-06-08 14:05:19 +0300

    proifle twitter

---
+++ CollectingData-SocialNetworks/showProfileT.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CollectingData_SocialNetworks
+{
+    /// <summary>
+    /// Interaction logic for showProfileT.xaml
+    /// </summary>
+    public partial class showProfileT : Window
+    {
+        public showProfileT()
+        {
+            InitializeComponent();
+        }
+    }
+}
